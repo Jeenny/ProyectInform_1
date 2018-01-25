77e79b85accfc141da2b0d85c7906e00f82bf9f9
ad2946 <adrigarort@gmail.com>
2018-01-25 17:27:22 +0100

    Modificacion VentanaLogin
    Cambio cuadro texto y tamaño boton

--- Proyecto_VitaSignal/src/View/VentanaLogin.java
+++ Proyecto_VitaSignal/src/View/VentanaLogin.java
@@ -12,7 +12,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.SpringLayout;
-<<<<<<< HEAD
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
@@ -20,22 +20,23 @@ import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-=======
+
 import javax.swing.JPasswordField;
->>>>>>> 0905e86cfee9f21e26370fdcbf011b168854753a
+
 
 public class VentanaLogin extends JFrame {
 
 	private JPanel contentPane;
-<<<<<<< HEAD
+	private JPanel contentPane_1;
+
 	private JTextField textField;
 	private JPasswordField passwordField;
-=======
+
 	
 	Color degradado = new Color(133,155,196);
 	
 
->>>>>>> 0905e86cfee9f21e26370fdcbf011b168854753a
+
 
 	/**
 	 * Launch the application.
@@ -58,15 +59,15 @@ public class VentanaLogin extends JFrame {
 	 */
 	public VentanaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-<<<<<<< HEAD
+
 		setBounds(415, 190, 530, 370);
 		setResizable(false);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(195, 211, 236));
-=======
+
 		setBounds(100, 100, 450, 300);
 		
-		contentPane = new JPanel() {
+		contentPane_1 = new JPanel() {
 			 @Override
 		        protected void paintComponent(Graphics grphcs) {
 		            super.paintComponent(grphcs);
@@ -81,50 +82,48 @@ public class VentanaLogin extends JFrame {
 			 
 		};
 		
->>>>>>> 0905e86cfee9f21e26370fdcbf011b168854753a
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+
+		contentPane_1.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane_1);
 		SpringLayout sl_contentPane = new SpringLayout();
-		contentPane.setLayout(sl_contentPane);
-<<<<<<< HEAD
+		contentPane_1.setLayout(sl_contentPane);
+
 		
 		JLabel lblUsuario = new JLabel("Usuario");
-		sl_contentPane.putConstraint(SpringLayout.WEST, lblUsuario, 96, SpringLayout.WEST, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblUsuario, -163, SpringLayout.SOUTH, contentPane);
-		contentPane.add(lblUsuario);
+		sl_contentPane.putConstraint(SpringLayout.WEST, lblUsuario, 96, SpringLayout.WEST, contentPane_1);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblUsuario, -163, SpringLayout.SOUTH, contentPane_1);
+		contentPane_1.add(lblUsuario);
 		
 		textField = new JTextField();
-		sl_contentPane.putConstraint(SpringLayout.NORTH, textField, 1, SpringLayout.NORTH, lblUsuario);
-		sl_contentPane.putConstraint(SpringLayout.WEST, textField, 64, SpringLayout.EAST, lblUsuario);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, textField, -153, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, textField, -102, SpringLayout.EAST, contentPane);
-		contentPane.add(textField);
+		sl_contentPane.putConstraint(SpringLayout.NORTH, textField, -3, SpringLayout.NORTH, lblUsuario);
+		sl_contentPane.putConstraint(SpringLayout.EAST, textField, -130, SpringLayout.EAST, contentPane_1);
+		contentPane_1.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblContrasea = new JLabel("Contraseña");
 		sl_contentPane.putConstraint(SpringLayout.NORTH, lblContrasea, 37, SpringLayout.SOUTH, lblUsuario);
 		sl_contentPane.putConstraint(SpringLayout.WEST, lblContrasea, 0, SpringLayout.WEST, lblUsuario);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblContrasea, -111, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, lblContrasea, -273, SpringLayout.EAST, contentPane);
-		contentPane.add(lblContrasea);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, lblContrasea, -111, SpringLayout.SOUTH, contentPane_1);
+		sl_contentPane.putConstraint(SpringLayout.EAST, lblContrasea, -273, SpringLayout.EAST, contentPane_1);
+		contentPane_1.add(lblContrasea);
 		
 		JButton btnAceptar = new JButton("Entrar");
-		sl_contentPane.putConstraint(SpringLayout.NORTH, btnAceptar, 35, SpringLayout.SOUTH, lblContrasea);
-		sl_contentPane.putConstraint(SpringLayout.WEST, btnAceptar, 0, SpringLayout.WEST, textField);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnAceptar, -30, SpringLayout.SOUTH, contentPane);
-		sl_contentPane.putConstraint(SpringLayout.EAST, btnAceptar, -218, SpringLayout.EAST, contentPane);
+		sl_contentPane.putConstraint(SpringLayout.WEST, btnAceptar, 168, SpringLayout.WEST, contentPane_1);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, btnAceptar, -32, SpringLayout.SOUTH, contentPane_1);
+		sl_contentPane.putConstraint(SpringLayout.EAST, btnAceptar, -178, SpringLayout.EAST, contentPane_1);
+		sl_contentPane.putConstraint(SpringLayout.WEST, textField, 0, SpringLayout.WEST, btnAceptar);
 		btnAceptar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		contentPane.add(btnAceptar);
+		contentPane_1.add(btnAceptar);
 		
 		passwordField = new JPasswordField();
+		sl_contentPane.putConstraint(SpringLayout.NORTH, btnAceptar, 23, SpringLayout.SOUTH, passwordField);
+		sl_contentPane.putConstraint(SpringLayout.SOUTH, passwordField, -106, SpringLayout.SOUTH, contentPane_1);
+		sl_contentPane.putConstraint(SpringLayout.EAST, passwordField, 0, SpringLayout.EAST, textField);
 		sl_contentPane.putConstraint(SpringLayout.WEST, passwordField, 7, SpringLayout.EAST, lblContrasea);
-		sl_contentPane.putConstraint(SpringLayout.SOUTH, passwordField, -25, SpringLayout.NORTH, btnAceptar);
-		sl_contentPane.putConstraint(SpringLayout.EAST, passwordField, 121, SpringLayout.EAST, lblContrasea);
-		contentPane.add(passwordField);
-=======
->>>>>>> 0905e86cfee9f21e26370fdcbf011b168854753a
+		contentPane_1.add(passwordField);
+
 	}
 }
