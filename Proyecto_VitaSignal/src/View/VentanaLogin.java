0905e86cfee9f21e26370fdcbf011b168854753a
ad2946 <adrigarort@gmail.com>
2018-01-25 17:21:53 +0100

    Degradado color ventana
    Creado metodo que pinta la ventana con un degradado de color

--- Proyecto_VitaSignal/src/View/VentanaLogin.java
+++ Proyecto_VitaSignal/src/View/VentanaLogin.java
@@ -1,15 +1,26 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+import javax.swing.JPasswordField;
 
 public class VentanaLogin extends JFrame {
 
 	private JPanel contentPane;
+	
+	Color degradado = new Color(133,155,196);
+	
+
 
 	/**
 	 * Launch the application.
@@ -33,10 +44,26 @@ public class VentanaLogin extends JFrame {
 	public VentanaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+		
+		contentPane = new JPanel() {
+			 @Override
+		        protected void paintComponent(Graphics grphcs) {
+		            super.paintComponent(grphcs);
+		            Graphics2D g2d = (Graphics2D) grphcs;
+		            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+		                    RenderingHints.VALUE_ANTIALIAS_ON);
+		            GradientPaint gp = new GradientPaint(0, 0,
+		                    Color.WHITE, 0, getHeight(),degradado);
+		            g2d.setPaint(gp);
+		            g2d.fillRect(0, 0, getWidth(), getHeight()); 
+		        }
+			 
+		};
+		
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		SpringLayout sl_contentPane = new SpringLayout();
+		contentPane.setLayout(sl_contentPane);
 	}
 
 }
